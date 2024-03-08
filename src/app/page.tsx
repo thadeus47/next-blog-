df40ef5c2a86d5328306a39406e69b965654317d
Thadeus Miseda <tmiseda@gmail.com>
2024-03-08 22:31:24 +0300

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@
 
 export default function Home() {
   return (
-    <main >
-  
+    <main className="text-center pt-32 px-5" >
+     <h1 className="first-heading">
+      Welcome to my blog
+     </h1>
+     <p className="max-w-[750px] mx-auto leading-8"></p>
   </main>
   );
 }
