e44ab5996c4d2151b4da2bb18b655ff7f3fa8f8d
Thadeus Miseda <tmiseda@gmail.com>
2024-03-15 23:49:02 +0300

    changes

---
+++ src/app/index.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+export default function HomePage() {
+    return (
+      <div>
+        <h1>Welcome to My Blog!</h1>
+        <ul>
+          
+        </ul>
+      </div>
+    );
+  }
+  
+  export async function getStaticProps() {
+    
+  }
+  
