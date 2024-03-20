84898f4a8e6c87f597aed72ba47dd2bb5c8a1719
Thadeus Miseda <tmiseda@gmail.com>
2024-03-20 23:57:52 +0300

    shadcn/ui components

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar(){
     return(
         <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
            <Link href="/" className="font-bold text-3xl">
-              Thadeus<span className="text-blue-500">Blog</span>
+              Thadeus<span className="text-primary">Blog</span>
            </Link>
         </nav>
     )
