f96f06d28c0e0ab7dd18ed61c346b21131c8d23b
Thadeus Miseda <tmiseda@gmail.com>
2024-03-22 16:37:45 +0300

    rendering the blog posts on index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
-import { Card } from "../components/ui/card";
+import { Card, CardContent } from "../components/ui/card";
 import { simpleBlogCard } from "./lib/interface";
 import { client, urlFor } from "./lib/sanity";
+import { Button } from "../components/ui/button";
+import Link from "next/link";
 
 async function getData() {
   const query = `
   *[_type == 'blog'] | order(_createdAt desc) {
     title,
-      smallDescription,
+      description,
       "currentSlug": slug.current,
       titleimage
   }`;
@@ -22,7 +24,7 @@ export default async function Home() {
 
   console.log(data);
   return (
-   <div className="grid grid-cols-1 lg:grid-cols-4 mt-5">
+   <div className="grid grid-cols-1  md:grid-cols-2 mt-5 gap-5">
       {data.map((post,idx) => (
         <Card key={idx}>
          <Image 
@@ -30,7 +32,15 @@ export default async function Home() {
            alt="image" 
            width={500}
            height={500}
+           className="rounded-t-lg h-[200px] object-cover"
            />
+         <CardContent className="mt-5">
+            <h3 className="text-lg line-clamp-2">{post.title}</h3>
+            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-200">{post.description}</p>
+            <Button asChild className="w-full mt-7">
+              <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
+            </Button>
+         </CardContent>
         </Card>
       ))}
    </div>
