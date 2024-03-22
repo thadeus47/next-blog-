a60f5dda7b14ef873332ec62d7cc8ae111488a3d
Thadeus Miseda <tmiseda@gmail.com>
2024-03-22 17:25:25 +0300

    rendering individual blog posts on page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,4 +1,7 @@
-import { client } from "../../lib/sanity";
+import Image from "next/image";
+import { fullBlog } from "../../lib/interface";
+import { client, urlFor } from "../../lib/sanity";
+import { PortableText } from "next-sanity";
 
 async function getData(slug: string) {
     const query = `
@@ -13,10 +16,27 @@ async function getData(slug: string) {
       return data;
 }
 export default async function BlogArticle({ params}: { params:{slug: string}}) {
-    const data = await getData(params.slug);
-    
+    const data: fullBlog = await getData(params.slug);
+
 
     return(
-        <h1>{params.slug}</h1>
+        <div className="mt-8 ">
+            <h1>
+                <span className="block text-base text-center text-primary font-semibold tracking-wide">Thadeus Miseda - Blog</span>
+                <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">{data.title}</span>
+            </h1>
+
+            <Image src={urlFor(data.titleimage).url()} 
+               width={800}  
+               height={800} 
+               alt="Title image" 
+               priority 
+               className="rounded-lg mt-8 border"
+            />
+
+            <div className="mt-16">
+                <PortableText value={data.content}/>
+            </div>
+        </div>
     )
 }
